e81a1c703d32b58676e16c2afe6147dff7037536
Angela Yepes <angelyep76@gmail.com>
2024-08-26 07:37:34 -0500

    setter y getters de clase

--- src/edu/sena/Relacion_entre_clases/model/Cliente.java
+++ src/edu/sena/Relacion_entre_clases/model/Cliente.java
@@ -3,4 +3,20 @@ package edu.sena.Relacion_entre_clases.model;
 public class Cliente {
     private String nombre;
     private String nif; // Número de Identificación Fiscal
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
 }
--- src/edu/sena/Relacion_entre_clases/model/Producto.java
+++ src/edu/sena/Relacion_entre_clases/model/Producto.java
@@ -4,4 +4,6 @@ public class Producto {
     private String codigo;
     private String nombre;
     private double precio;
+
+
 }
