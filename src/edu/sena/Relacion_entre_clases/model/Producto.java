21891a391e9ce2dadb3b2eb273e9a64d524be590
Angela Yepes <angelyep76@gmail.com>
2024-08-27 15:10:22 -0500

    New Constructor Producto

--- src/edu/sena/Relacion_entre_clases/model/Producto.java
+++ src/edu/sena/Relacion_entre_clases/model/Producto.java
@@ -1,16 +1,23 @@
 package edu.sena.Relacion_entre_clases.model;
 
 public class Producto {
-    private String codigo;
+    private int codigo;
     private String nombre;
     private double precio;
     private static int ultimoCodigo;
 
-    public String getCodigo() {
+    public Producto(String nombre, double precio){
+        ultimoCodigo++;
+        this.codigo = ultimoCodigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(String codigo) {
+    public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
 
@@ -36,5 +43,13 @@ public class Producto {
 
     public static void setUltimoCodigo(int ultimoCodigo) {
         Producto.ultimoCodigo = ultimoCodigo;
+    }
+
+    @Override
+    public String toString() {
+
+        return codigo +
+                "\t" + nombre +
+                "\t" + precio;
     }
 }
