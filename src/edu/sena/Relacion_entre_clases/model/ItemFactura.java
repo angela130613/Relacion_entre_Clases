c83d60d2eb5502a8028539831dfaee60a3d4ac54
Angela Yepes <angelyep76@gmail.com>
2024-08-27 14:27:07 -0500

    setter y getters ItemFactura

--- src/edu/sena/Relacion_entre_clases/model/ItemFactura.java
+++ src/edu/sena/Relacion_entre_clases/model/ItemFactura.java
@@ -3,4 +3,20 @@ package edu.sena.Relacion_entre_clases.model;
 public class ItemFactura {
     private Producto producto;
     private int cantidad;
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
