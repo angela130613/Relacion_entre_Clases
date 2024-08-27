5aafcbb3dbd6e403c1cce3168b21e043ed98ea25
Angela Yepes <angelyep76@gmail.com>
2024-08-27 14:23:13 -0500

    setter y getters de Producto

--- src/edu/sena/Relacion_entre_clases/model/Producto.java
+++ src/edu/sena/Relacion_entre_clases/model/Producto.java
@@ -5,5 +5,27 @@ public class Producto {
     private String nombre;
     private double precio;
 
+    public String getCodigo() {
+        return codigo;
+    }
 
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
