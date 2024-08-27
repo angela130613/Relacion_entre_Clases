58d7c871c9650d96c79dcac25ac18eea66a832d2
Angela Yepes <angelyep76@gmail.com>
2024-08-27 14:33:44 -0500

    setter y getters Factura

--- src/edu/sena/Relacion_entre_clases/model/Factura.java
+++ src/edu/sena/Relacion_entre_clases/model/Factura.java
@@ -8,4 +8,44 @@ public class Factura {
     private Date fecha;
     private Cliente cliente;
     private ItemFactura[] items;
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ItemFactura[] getItems() {
+        return items;
+    }
+
+    public void setItems(ItemFactura[] items) {
+        this.items = items;
+    }
 }
