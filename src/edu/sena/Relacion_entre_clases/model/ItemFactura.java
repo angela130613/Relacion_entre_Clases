5d063e2fc6e585af91f8e23846acb0f077601ee2
Angela Yepes <angelyep76@gmail.com>
2024-08-27 15:17:24 -0500

    Constructor ItemFactura

--- src/edu/sena/Relacion_entre_clases/model/ItemFactura.java
+++ src/edu/sena/Relacion_entre_clases/model/ItemFactura.java
@@ -4,6 +4,11 @@ public class ItemFactura {
     private Producto producto;
     private int cantidad;
 
+    public ItemFactura(Producto producto, int cantidad){
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
     public Producto getProducto() {
         return producto;
     }
